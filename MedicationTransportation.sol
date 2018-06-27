0010f404a71663ac56a4b9e76e1f23fe99925625
Ryan Lowdermilk <ryanlowdermilk@gmail.com>
2018-06-27 15:30:15 -0500

    init

---
+++ MedicationTransportation.sol
@@ -0,0 +1,92 @@
+pragma solidity ^0.4.20;
+
+contract WorkbenchBase {
+    event WorkbenchContractCreated(string applicationName, string workflowName, address originatingAddress);
+    event WorkbenchContractUpdated(string applicationName, string workflowName, string action, address originatingAddress);
+
+    string internal ApplicationName;
+    string internal WorkflowName;
+
+    function WorkbenchBase(string applicationName, string workflowName) internal {
+        ApplicationName = applicationName;
+        WorkflowName = workflowName;
+    }
+
+    function ContractCreated() internal {
+        WorkbenchContractCreated(ApplicationName, WorkflowName, msg.sender);
+    }
+
+    function ContractUpdated(string action) internal {
+        WorkbenchContractUpdated(ApplicationName, WorkflowName, action, msg.sender);
+    }
+}
+
+contract MedicationTransportation is WorkbenchBase('MedicationTransportation', 'MedicationTransportation') {
+
+    //Set of States
+    enum StateType { MedicationCreated, TransferredToWholesale, TransferredToDistribution, PossessedByPharmacy, Completed, InTransit}
+    
+    //List of properties
+    StateType public  State;
+    address public  Owner;
+    address public  InitiatingCounterparty;
+    address public  Counterparty;
+    address public  PreviousCounterparty;
+    address public  SupplyChainOwner;
+    address public  SupplyChainObserver;
+    
+    function MedicationTransportation(address supplyChainOwner, address supplyChainObserver) public
+    {
+        InitiatingCounterparty = msg.sender;
+        Owner = InitiatingCounterparty;
+        Counterparty = InitiatingCounterparty;
+        SupplyChainOwner = supplyChainOwner;
+        SupplyChainObserver = supplyChainObserver;
+        State = StateType.MedicationCreated;
+        ContractCreated();
+    }
+
+    function TransferResponsibility(address newCounterparty) public
+    {
+        // keep the state checking, message sender, and device checks separate
+        // to not get cloberred by the order of evaluation for logical OR
+        if ( State == StateType.Completed )
+        {
+            revert();
+        }
+
+        if ( InitiatingCounterparty != msg.sender && Counterparty != msg.sender )
+        {
+            revert();
+        }
+
+        if (State == StateType.MedicationCreated)
+        {
+            State = StateType.InTransit;
+        }
+
+        PreviousCounterparty = Counterparty;
+        Counterparty = newCounterparty;
+        ContractUpdated('TransferResponsibility');
+    }
+
+    function Complete() public
+    {
+        // keep the state checking, message sender, and device checks separate
+        // to not get cloberred by the order of evaluation for logical OR
+        if ( State == StateType.Completed )
+        {
+            revert();
+        }
+
+        if (Owner != msg.sender && SupplyChainOwner != msg.sender)
+        {
+            revert();
+        }
+
+        State = StateType.Completed;
+        PreviousCounterparty = Counterparty;
+        Counterparty = 0x0;
+        ContractUpdated('Complete');
+    }
+}
